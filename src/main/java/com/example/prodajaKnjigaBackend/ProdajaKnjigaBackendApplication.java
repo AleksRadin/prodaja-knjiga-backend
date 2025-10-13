322ec58d67bf4b4faf846172852a4a72b47bda4e
aleksandar-radin <ar20190328@student.fon.bg.ac.rs>
2025-10-13 16:08:33 +0200

    Initial commit

---
+++ src/main/java/com/example/prodajaKnjigaBackend/ProdajaKnjigaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.prodajaKnjigaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdajaKnjigaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdajaKnjigaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prodajaKnjigaBackend/ProdajaKnjigaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prodajaKnjigaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdajaKnjigaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
